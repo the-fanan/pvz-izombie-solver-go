cef92b35102ef8f910eb15359f0e2de626c06aa6
Fanan Moses Dala <fanan@meta.com>
2026-05-21 11:23:57 +0100

    feat: use velocity for angle and not max height
    this way we get to control the velocity

--- simulator/dimensions.go
+++ simulator/dimensions.go
@@ -3,8 +3,6 @@ package simulator
 import "math"
 
 const CELL_LENGTH_MICRO_METER = float64(1180122)
-const MAX_HEIGHT_FOR_POLE_VAULT_ZOMBIE_MICRO_METER = float64(810000)
-const MAX_HEIGHT_FOR_CATAPULT_ITEMS_IN_MICRO_METER = float64(2000000)
 const TIME_TO_TRAVERSE_CELL_FOR_FASTEST_OBJECT_MILLI_SECOND = float64(250)
 
 func SubCellsPerMicroMeter(resolution int) float64 {
@@ -15,10 +13,6 @@ func FramePerMilliSecond(resolution int) float64 {
 	return float64(resolution)/ TIME_TO_TRAVERSE_CELL_FOR_FASTEST_OBJECT_MILLI_SECOND
 }
 
-func MaxProjectileHeightInSubCells(maxHeightInMicrometer float64, resolution int) float64 {
-	return (maxHeightInMicrometer / CELL_LENGTH_MICRO_METER) * float64(resolution)
-}
-
 func AccelerationDueToGravityInSubcellsPerSquareFrames(resolution int) float64 {
 	fpm := FramePerMilliSecond(resolution)
 	spm := SubCellsPerMicroMeter(resolution)
@@ -26,13 +20,12 @@ func AccelerationDueToGravityInSubcellsPerSquareFrames(resolution int) float64 {
 	return (9.8 * spm) / math.Pow(fpm, 2)
 }
 
-func StartingAngleForProjectileMotion(maxHeightInSubCells, distanceInSubCells float64) float64 {
-	return math.Atan((4 * maxHeightInSubCells) / distanceInSubCells)
-}
-
-func ProjectileVelocity(resolution int, maxHeightInSubCells, angle float64) float64 {
+func AngleForProjectileMotion(resolution int, velocity, projectilRange float64) float64 {
 	adtg := AccelerationDueToGravityInSubcellsPerSquareFrames(resolution)
-	return math.Sqrt((2 * maxHeightInSubCells * adtg) / math.Pow(math.Sin(angle), 2))
+	v := (adtg * projectilRange) / math.Pow(velocity, 2)
+	angle := 0.5 * math.Asin(v)
+
+	return angle
 }
 
 func ProjectileVerticalDisplacement(resolution, frame int, velocity, angle float64) float64 {
